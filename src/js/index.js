9aae4b10f611cb785caa2e8c10b3f06a8f8def35
BarrySchutte <Barry_Schutte@live.nl>
2020-01-13 13:07:49 +0100

    recipe model

--- src/js/index.js
+++ src/js/index.js
@@ -15,45 +15,78 @@ const state = {};
 // ----- SEARCH CONTROLLER -----
 
 const controlSearch = async () => {
-    // 1. Get query from view
-    const query = searchView.getInput();
+  // 1. Get query from view
+  const query = searchView.getInput();
 
-    if (query) {
-        // 2. New search object and add to state
-        state.search = new Search(query);
+  if (query) {
+    // 2. New search object and add to state
+    state.search = new Search(query);
 
-        // 3. Prepare UI for results
-        searchView.clearInput();
-        searchView.clearResults();
-        renderLoader(elements.searchRes);
+    // 3. Prepare UI for results
+    searchView.clearInput();
+    searchView.clearResults();
+    renderLoader(elements.searchRes);
 
-        // 4. Search for recipes
-        await state.search.getResults();
+    try {
+      // 4. Search for recipes
+      await state.search.getResults();
 
-        // 5. Render results on UI
-        clearLoader();
-        searchView.renderResults(state.search.result);
+      // 5. Render results on UI
+      clearLoader();
+      searchView.renderResults(state.search.result);
+    } catch (error) {
+      alert('Something went wrong with the search');
+      clearLoader();
     }
+  }
 };
 
 elements.searchForm.addEventListener('submit', e => {
-    e.preventDefault();
-    controlSearch();
+  e.preventDefault();
+  controlSearch();
 });
 
 elements.searchResPages.addEventListener('click', e => {
-    const btn = e.target.closest('.btn-inline');
-    if(btn) {
-        const goToPage = parseInt(btn.dataset.goto, 10);
-        searchView.clearResults();
-        searchView.renderResults(state.search.result, goToPage);
-    }
+  const btn = e.target.closest('.btn-inline');
+  if (btn) {
+    const goToPage = parseInt(btn.dataset.goto, 10);
+    searchView.clearResults();
+    searchView.renderResults(state.search.result, goToPage);
+  }
 });
-
-
 
 // ----- RECIPE CONTROLLER -----
 
-const r = new Recipe(46956);
-r.getRecipe(); 
-console.log(r);
+const controlRecipe = async () => {
+  // Get ID from url
+  const id = window.location.hash.replace('#', '');
+
+  if (id) {
+    // Prepare UI for changes
+
+    // Create new recipe object and add to state
+    state.recipe = new Recipe(id);
+
+    try {
+      // Get recipe data and parse ingredients
+      await state.recipe.getRecipe();
+      console.log(state.recipe.ingredients);
+      state.recipe.parseIngredients();
+
+      // Calculate servings and time
+      state.recipe.calcTime();
+      state.recipe.calcServings();
+
+      // Render recipe
+      console.log(state.recipe);
+    } catch (err) {
+      alert('Error processing recipe!');
+    }
+  }
+};
+
+// window.addEventListener('hashchange', controlRecipe);
+// window.addEventListener('load', controlRecipe);
+['hashchange', 'load'].forEach(event =>
+  window.addEventListener(event, controlRecipe)
+);
